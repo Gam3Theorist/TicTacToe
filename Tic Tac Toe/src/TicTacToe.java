935dbfdd14c46fe0a998faf480736aea5dd9ba0f
adam.ward <adam.ward@MU-C1-15.mullen.pvt.k12.co.us>
2025-09-26 12:31:39 -0600

    Finished Board setup
    -Must fix player choice, changes to X before can look if its empty
    -Must make choice before changing to x to check if slot is empty

--- Tic Tac Toe/src/TicTacToe.java
+++ Tic Tac Toe/src/TicTacToe.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 public class TicTacToe
 
 	{
-		static String[][] board = new String[3][3];
+	static String[][] board = new String[3][3];
+	static Scanner userInput = new Scanner(System.in);
+	static String choice= userInput.next();
 		
 		public static void main(String[] args)
 			{
+				System.out.println("Welcome to TicTacToe! \n Beat the computer \n+---+---+---+---+ \nAnswer number, then letter (Example: 1A)  ");
+				
+		CoinFlip();
 		FillBoard();
-		board();
+		displayBoard();
 
 		
 				
@@ -18,42 +23,112 @@ public class TicTacToe
 		
 		//=====================================================================================================================================================
 		
+		public static void CoinFlip()
+		{
+			int coin = ((int)(Math.random()*2)+1);
+			//System.out.println(coin);
+		}
+		
+		public static void Choice()
+		{
+			
+			
+		}
+		
 		public static void FillBoard()
 		{
 		
 		
-			for(int row=0; row<board[].length; row++)
+			for(int row=0; row < board.length; row++)
 				{
-					
+					for(int col=0; col < board[row].length; col++)
+						{
+							board[row][col]=" ";
+						}
 				}
+		//	System.out.println("Fill test");
 		}
-		
-		
-		
-		
-		public static void board()
+	
+		public static void displayBoard()
 			{
 				
 
 								
-			board[0][0]="";
-			board[0][1]="";
-			board[0][2]="";
+		
+			System.out.println("    A   B   C  ");
+			System.out.println("  +---+---+---+");
+			System.out.println("1 | "+board[0][0]+" | "+board[0][1]+" | "+board[0][2]+" |");
+			System.out.println("  +---+---+---+");	
+			System.out.println("2 | "+board[1][0]+" | "+board[1][1]+" | "+board[1][2]+" |");
+			System.out.println("  +---+---+---+");
+			System.out.println("3 | "+board[2][0]+" | "+board[2][1]+" | "+board[2][2]+" |");
+			System.out.println("  +---+---+---+");
+		}
+	
+		public static void userPlay()
+		{
 			
-			board[1][0]="";
-			board[1][1]="";
-			board[1][2]="";
+			boolean valid=false;
 			
-			board[2][0]="";
-			board[2][1]="";
-			board[2][2]	="";
+			System.out.println("You're turn!");
 			
-			System.out.println("+---+---+---+");
-			System.out.println("| "+board[0][0]+" | "+board[0][1]+" | "+board[0][2]+" |");
-			System.out.println("+---+---+---+");	
-			System.out.println("| "+board[1][0]+" | "+board[1][1]+" | "+board[1][2]+" |");
-			System.out.println("+---+---+---+");
-			System.out.println("| "+board[2][0]+" | "+board[2][1]+" | "+board[2][2]+" |");
-			System.out.println("+---+---+---+");
-			}
+			while(valid==false)
+				{
+					
+					String choiceArray[] = choice.split("");
+					
+					if(choiceArray[0].equals("1"))
+						{
+							if(choiceArray[1].equals("A"))
+								{
+									board[0][0]="X";
+								}
+							else if(choiceArray[1].equals("B"))
+								{
+									board[0][1]="X";
+								}
+							else if(choiceArray[1].equals("C"))
+								{
+									board[0][2]="X";
+								}
+						}
+					else if (choiceArray[0].equals("2"))
+						{
+							if(choiceArray[1].equals("A"))
+								{
+									board[1][0]="X";
+								}
+							else if(choiceArray[1].equals("B"))
+								{
+									board[1][1]="X";
+								}
+							else if(choiceArray[1].equals("C"))
+								{
+									board[1][2]="X";
+								}
+						}
+					else if (choiceArray[0].equals("3"))
+						{
+							if(choiceArray[1].equals("A"))
+								{
+									board[2][0]="X";
+								}
+							else if(choiceArray[1].equals("B"))
+								{
+									board[2][1]="X";
+								}
+							else if(choiceArray[1].equals("C"))
+								{
+									board[2][2]="X";
+								}
+						}
+				
+					
+					
+				}
+			
+		}
+		
+		
+		
 	}
